ba8fbf0738687d230d23457a29d48b61a10c15e4
Pranav <pranavsresh1947@gmail.com>
2026-04-16 19:15:59 +0530

    Fix: Removed Redundant files

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,3 @@
-import Education from "./Education";
-
 export default function About() {
     return (
         <section className="min-h-screen max-w-screen bg-black text-white flex flex-col items-center justify-center gap-6" id="about">
@@ -16,16 +14,47 @@ export default function About() {
                 year="2021 - 2023"
                 title="Higher Secondary Education"
                 grade="92.2%"
-                place="Sishya Institute of Education"
+                place="Sishya School of Hosur"
                 description="Focused on Physics, Chemistry, and Mathematics with strong analytical problem-solving and conceptual understanding."
             />
             <Education
                 year="2011 - 2021"
                 title="High School Education"
                 grade="95.34%"
-                place="Asian Christian High School"
+                place="Asian Christian Academy of India"
                 description="Built a strong academic foundation with consistent performance and active participation in academics and extracurricular activities."
             />
         </section>
+    );
+}
+
+function Education({
+    year,
+    title,
+    place,
+    description,
+    grade
+}) {
+    return (
+        <div className="border border-gray-700 rounded-xl p-5 bg-white/5 backdrop-blur-md hover:border-white transition duration-300">
+            {/* Year */}
+            <p className="text-sm text-gray-400 mb-1">{year}</p>
+            {/* Title */}
+            <h2 className="text-xl font-semibold">{title}</h2>
+            {/* GRADE */}
+            {grade && (
+                <p className="text-gray-400 mt-2 text-sm">
+                    grade: {grade}
+                </p>
+            )}
+            {/* Place */}
+            <p className="text-gray-300">{place}</p>
+            {/* Description */}
+            {description && (
+                <p className="text-gray-400 mt-2 text-sm">
+                    {description}
+                </p>
+            )}
+        </div>
     );
 }
